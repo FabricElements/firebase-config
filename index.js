2e880ab65e15f9912a338a5617e065ec368fa237
Erny Sans <ernysans@icloud.com>
2018-06-06 21:25:45 -0500

    Base config

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Basic firebase init for FabricElements
+ */
+(function() {
+  'use strict';
+  if (typeof firebase === 'undefined') {
+    throw new Error('hosting/init-error: Firebase SDK not detected.');
+  }
+  // Initialize Firebase
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCJRNdR0eodnswyi8MHCtF1YOjY235mhM8',
+    authDomain: 'fabricelements.firebaseapp.com',
+    databaseURL: 'https://fabricelements.firebaseio.com',
+    projectId: 'fabricelements',
+    storageBucket: 'fabricelements.appspot.com',
+    messagingSenderId: '908593247251',
+  });
+  // set data property on dom-bind
+  let autobind = document.querySelector('dom-bind');
+  if (autobind) {
+    firebase.auth().onAuthStateChanged((user) => {
+      autobind.user = user;
+      autobind.signedIn = !(!user);
+    });
+  }
+})();
